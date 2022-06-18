11ce9226e7291559b8dbee52dfe68b208a1b4b62
ZuhalAlanlar <zuhalalanlar@gmail.com>
2022-06-19 02:58:56 +0300

    login register actions views oluşturuldu

--- mvc-ticket-theater/Controllers/AccountController.cs
+++ mvc-ticket-theater/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using mvc_ticket_theater.Data;
+using mvc_ticket_theater.Data.Static;
 using mvc_ticket_theater.Data.ViewModels;
 using mvc_ticket_theater.Models;
 using System;
@@ -29,5 +30,61 @@ namespace mvc_ticket_theater.Controllers
 
             return View(new LoginVM());
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (!ModelState.IsValid) 
+                return View(loginVM);
+
+         
+
+            var user = await userManager.FindByEmailAsync(loginVM.EmailAdress);
+
+            if (user ! == null)
+            {
+                var passwordCheck = await userManager.CheckPasswordAsync(user, loginVM.Password);
+                if(passwordCheck){
+                    var result = await signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if(result.Succeeded)
+                    { return RedirectToAction("Index", "Theaters");
+                    }
+                }
+                TempData["Error"] = "Girilen Bilgiler Yanlış!";
+                return View(loginVM);
+            }
+            TempData["Error"] = "Girilen Bilgiler Yanlış!";
+            return View(loginVM);
+        }
+
+
+        public IActionResult Register() => View(new RegisterVM());
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await userManager.FindByEmailAsync(registerVM.EmailAdress);
+            if (user != null)
+            {
+                TempData["Error"] = "Email Adresi Sistemde Var";
+                return View(registerVM);
+            }
+
+            var newUser = new AppUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAdress,
+                UserName = registerVM.EmailAdress
+            };
+            var newUserResponse = await userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded)
+                await userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
+
     }
 }
--- mvc-ticket-theater/Data/ViewModels/RegisterVM.cs
+++ mvc-ticket-theater/Data/ViewModels/RegisterVM.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace mvc_ticket_theater.Data.ViewModels
 {
-    public class LoginVM
+    public class RegisterVM
     {
+        [Display(Name = "İsim Soyisim")]
         [Required]
+        public string FullName { get; set; }
+
+
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "Mail adresini giriniz.")]
+
         public string EmailAdress { get; set; }
 
         [Required]
         [Display(Name = "Parola")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Parola Onayla")]
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Farklı Parola Girdiniz")]
+        public string ConfirmPassword { get; set; }
+  
 
 
 
